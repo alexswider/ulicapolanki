27712352d9f1ac6f402d3a14d9b94c7ceb22c7f5
Alex Swiderski <alex@fpo.studio>
2023-04-23 15:47:54 +0200

    first

---
+++ ulicapolanki/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <iframe src="https://gamma.app/embed/n9be2twkeldp7o9" style="width: 700px; max-width: 100%; height: 100vh" allow="fullscreen" title="Polanki Street Community in Poland"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
